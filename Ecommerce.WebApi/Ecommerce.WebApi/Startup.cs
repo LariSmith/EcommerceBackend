2b7c78732c84359a297bfd1a50f7c402e830da17
LariSmith <lena.dracarys@gmail.com>
2021-05-15 12:49:49 -0300

    adicionando service em Startup

--- Ecommerce.WebApi/Ecommerce.WebApi/Startup.cs
+++ Ecommerce.WebApi/Ecommerce.WebApi/Startup.cs
@@ -1,14 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Ecommerce.Service.Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Ecommerce.WebApi
 {
@@ -25,6 +20,8 @@ namespace Ecommerce.WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<EcommerceContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
