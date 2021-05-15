747ab8714eeb5297e91640e5506c6d72c62d7463
LariSmith <lena.dracarys@gmail.com>
2021-05-15 13:08:15 -0300

    criando Migration

---
+++ Ecommerce.WebApi/Ecommerce.Service/Migrations/20210515160733_initial.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ecommerce.Service.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "clientes",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    cpf = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_clientes", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produto",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    preco = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    descricao = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produto", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "pedidos",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    idCliente = table.Column<int>(type: "int", nullable: false),
+                    clienteid = table.Column<int>(type: "int", nullable: true),
+                    dataPedido = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_pedidos", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_pedidos_clientes_clienteid",
+                        column: x => x.clienteid,
+                        principalTable: "clientes",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "itens",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    id_pedido = table.Column<int>(type: "int", nullable: false),
+                    pedidoid = table.Column<int>(type: "int", nullable: true),
+                    idProduto = table.Column<int>(type: "int", nullable: false),
+                    Produtoid = table.Column<int>(type: "int", nullable: true),
+                    quantidade = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_itens", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_itens_pedidos_pedidoid",
+                        column: x => x.pedidoid,
+                        principalTable: "pedidos",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_itens_Produto_Produtoid",
+                        column: x => x.Produtoid,
+                        principalTable: "Produto",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_itens_pedidoid",
+                table: "itens",
+                column: "pedidoid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_itens_Produtoid",
+                table: "itens",
+                column: "Produtoid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_pedidos_clienteid",
+                table: "pedidos",
+                column: "clienteid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "itens");
+
+            migrationBuilder.DropTable(
+                name: "pedidos");
+
+            migrationBuilder.DropTable(
+                name: "Produto");
+
+            migrationBuilder.DropTable(
+                name: "clientes");
+        }
+    }
+}
