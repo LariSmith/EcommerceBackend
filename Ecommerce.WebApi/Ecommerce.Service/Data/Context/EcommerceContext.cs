6e00cb079b66edc7e7424a144e9a087fd733fca2
LariSmith <lena.dracarys@gmail.com>
2021-05-15 12:14:16 -0300

    criando context e map + nupackage

---
+++ Ecommerce.WebApi/Ecommerce.Service/Data/Context/EcommerceContext.cs
@@ -0,0 +1,21 @@
+﻿using Ecommerce.Service.Data.Map;
+using Ecommerce.Service.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecommerce.Service.Data.Context
+{
+    class EcommerceContext : DbContext
+    {
+        public DbSet<Produto> produtos { get; set; }
+
+        public EcommerceContext(DbContextOptions<EcommerceContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new EcommerceMap());
+        }
+    }
+}
