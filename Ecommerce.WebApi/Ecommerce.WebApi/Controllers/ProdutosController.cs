f2105f350c0c91d1b4c25e9cd907c3a1f4f6bf85
LariSmith <lena.dracarys@gmail.com>
2021-05-15 14:55:44 -0300

    criando controler Produto

---
+++ Ecommerce.WebApi/Ecommerce.WebApi/Controllers/ProdutosController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ecommerce.Service.Interface;
+using Ecommerce.Service.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly IService<Produto> service;
+
+        public ProdutosController(IService<Produto> service)
+        {
+            this.service = service;
+        }
+
+        public IEnumerable<Produto> ListarProdutos()
+        {
+            return service.Listar();
+        }
+    }
+}
